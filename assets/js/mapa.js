983421049ccbf187a5ba853327e0b89245d9eb9a
PauLeeR <lindaliel@gmail.com>
2017-07-03 00:00:32 -0400

    botones, marcador changed

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -1,54 +1,95 @@
-var map;   /*inicializo el mapa*/
+var center;
 var latitud;
 var longitud;
+var map = null;
 
-$(document).ready(function() {
-    initMap(); /* cargamos el mapa*/   
-});
+/*Control del redimensionamiento de la ventana*/
+window.onresize = function(){
+    document.getElementById("contenedor_mapa").style.height = (window.innerHeight) + "px";
+}
 
-function initMap() {
-    map = new google.maps.LatLng(latitud, longitud); /* Creamos un punto con nuestras coordenadas */
+/* Creo el mapa, y lo centro en las coordenadas de la ubicacion del usuario*/
+function initMap(){
+    var mapdivMap = document.getElementById("contenedor_mapa");
+    mapdivMap.style.width = (window.innerWidth);
+    mapdivMap.style.height = (window.innerHeight) + "px";
+    
+    center = new google.maps.LatLng(latitud, longitud);
     var myOptions = {
         zoom: 17,
-        center: map, /* Definimos la posicion del mapa con el punto */
+        center: center,/* Definimos la posicion del mapa con el punto */
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    }/*zoom del mapa y otras cosas*/
+    } /*zoom del mapa y otras cosas*/
+    map = new google.maps.Map(document.getElementById("contenedor_mapa"), myOptions);
+    findme();
+}
 
-    function findme() {
-        if (navigator.geolocation) { /* Si el navegador tiene geolocalizacion */
-            navigator.geolocation.getCurrentPosition(found, notFound);
-        }else{
-            alert('Oops! Tu navegador no soporta geolocalización. Bájate Chrome, que es gratis!');
-        }
-    }
+/*solicitud de geolocalización y ver si el navegador la soporta*/
 
-    window.addEventListener("load", findme); 
+function findme(){/* Si el navegador tiene geolocalizacion */
+    if(navigator.geolocation){
+        //alert ("Obteniendo posición...");
+        navigator.geolocation.getCurrentPosition(centrarMapa,errorPosicionar);
+    }else{
+        alert('Oops! Tu navegador no soporta geolocalización');
+    }   
+}
 
-    function found(position) {
-        latitud = position.coords.latitude; /*Guardamos la latitud*/
-        longitud = position.coords.longitude; /*Guardamos la longitud*/
-        initMap();
-        
-    };
-        //map.setZoom(17);
-       // map.setCenter({lat:latitud, lng:longitud});
-   
-    function notFound(err) {
+/*alerts en caso de errores*/
+
+function errorPosicionar(error) {
     /*alert en caso de error*/
     alert("Oops! Algo ha salido mal");
-    }
+}  
 
-    /*Esta funcion se ejecuta si la llamada a  navigator.geolocation.getCurrentPosition
-    tiene exito. La latitud y la longitud vienen dentro del objeto coords*/
-    function centrarMapa(pos, z){
-        map.setZoom(16);
-        map.setCenter(new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude));
-        var miUbicacion = new google.maps.Marker({/*Creamos un marcador*/
-            position: new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude),
-            animation: google.maps.Animation.DROP,
-            map: map, /* Lo vinculamos a nuestro mapa */
-            title:"Dónde estoy?"
-        });
-
-    }
+ /* Esta función se ejecuta si getCurrentPosition tiene éxito. La latitud y la longitud vienen dentro del objeto coords*/
+ 
+ function centrarMapa(pos, z){
+    map.setZoom(15);
+    map.setCenter(new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude));
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude),
+        title:"Usted está aquí",
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE, //cambiamos el marcador por defecto por uno de círculo
+          scale: 10
+      },
+      draggable: true,
+      map: map
+  });
 }
+
+/*DirectionsService y DirectionsDisplay se desprenden de google, son objetos de google
+        var ds = new google.maps.DirectionsService();//obtiene las coordenadas
+        var dr = new google.maps.DirectionsRenderer();//traduce esas coordenadas a la ruta visible (ruta en azul en el mapa)
+        //document.getElementById("origen").addEventListener("change", onChangeHandler);
+        document.getElementById("destino").addEventListener("change", onChangeHandler);
+        
+        function rutaVisible (ds, dr) {
+          ds.route({
+            origin: center,
+            destination: document.getElementById("pickup").value,
+            travelMode: "DRIVING"
+          },
+
+          function(response, status) {
+            if (status === "OK") {
+              dr.setDirections(response);
+            } else {
+              window.alert("Ruta "+ status);
+            }
+          });
+        }
+        
+//onChangeHandler = Agrega una propiedad de seguimiento a una definición de lenguaje específico de dominio
+        dr.setMap(contenedor_mapa);
+        var onChangeHandler = function(){
+            //Servicio de indicaciones
+            rutaVisible(ds, dr);
+        }; 
+
+        document.getElementById("pickup").addEventListener("click",onChangeHandler);
+        */
+
+
+
