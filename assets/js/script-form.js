eb42ce23b4e37a6901a6843986ea9129cd160c36
marcelaorellana <marcelapaz.1985@gmail.com>
2017-07-04 18:57:05 -0400

    cambia imagen usuario pero solo en editar perfil

--- assets/js/script-form.js
+++ assets/js/script-form.js
@@ -19,5 +19,29 @@ $(document).ready(function(){
 		localStorage.setItem("photo-profile", photo);
 
     });
+    $("#profileImage").on("change", previewFile);
 
 });
+function previewFile() {
+    // Where you will display your image
+    var preview = document.querySelector('#photo-profile');
+    // The button where the user chooses the local image to display
+    var file = document.querySelector('input[type=file]').files[0];
+    // FileReader instance
+    var reader  = new FileReader();
+
+    // When the image is loaded we will set it as source of
+    // our img tag
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+
+    
+    if (file) {
+      // Load image as a base64 encoded URI
+      reader.readAsDataURL(file);
+      console.log(file);
+    } else {
+      preview.src = "";
+    }
+  }
